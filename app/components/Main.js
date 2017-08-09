d9a12bb7f0fa4ce1c5273e6b02842bbc01a1dab9
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-08-08 20:15:37 -0700

    initial site files

---
+++ app/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//Import child components
+import Bio from "./children/Bio";
+import Contact from "./children/Contact";
+import Footer from "./children/Footer";
+import Header from "./children/Header";
+import Portfolio from "./children/Portfolio";
+import Sidebar from "./children/Sidebar";
+
+//Helper functions
+// import API from "../utils/API";
+
+// very basic component to get started
+const Main = () => (
+  <h1>Hello World!</h1>
+);
+
+export default Main;
